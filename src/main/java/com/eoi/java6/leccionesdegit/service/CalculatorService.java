74eefba0d5bccf23e6fd83fdcf6cd40e49bace6e
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-11 00:50:41 +0100

    Añadido CalculatorService en main (sin implementación)

---
+++ src/main/java/com/eoi/java6/leccionesdegit/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package com.eoi.java6.leccionesdegit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    // ❌ Falta implementar la lógica de la calculadora
+}
