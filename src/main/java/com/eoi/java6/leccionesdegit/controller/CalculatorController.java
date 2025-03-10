0087c101709c7e562ee072368f618c9724e897d7
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-11 00:51:17 +0100

    CalculatorController sin integración con CalculatorService en main

--- src/main/java/com/eoi/java6/leccionesdegit/controller/CalculatorController.java
+++ src/main/java/com/eoi/java6/leccionesdegit/controller/CalculatorController.java
@@ -8,6 +8,6 @@ public class CalculatorController {
 
     @GetMapping("/calculadora")
     public String mostrarCalculadora() {
-        return "Calculadora funcionando"; // ❌ Error: No devuelve la vista Thymeleaf
+        return "calculator";
     }
 }
