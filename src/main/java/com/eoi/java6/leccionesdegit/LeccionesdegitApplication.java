0ba3f424d12b05e14aa21bc97c8c21a0c8748467
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-11 00:07:28 +0100

    Initial commit

---
+++ src/main/java/com/eoi/java6/leccionesdegit/LeccionesdegitApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.java6.leccionesdegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeccionesdegitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeccionesdegitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eoi/java6/leccionesdegit/LeccionesdegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoi.java6.leccionesdegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeccionesdegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
