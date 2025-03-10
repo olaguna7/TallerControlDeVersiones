ab31a760223db97716e94ce6de8e8987503323c4
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2025-03-11 00:27:50 +0100

    Añadido Calculator.java con errores iniciales en main

--- src/main/java/com/eoi/java6/leccionesdegit/ejercicio1/Calculator.java
+++ src/main/java/com/eoi/java6/leccionesdegit/ejercicio1/Calculator.java
@@ -3,7 +3,7 @@ package com.eoi.java6.leccionesdegit.ejercicio1;
 public class Calculator {
 
     public int sumar(int a, int b) {
-        return a - b; // ❌ ERROR: Está mal implementado
+        return a - b; // ❌ ERROR: Implementación incorrecta
     }
 
     public int dividir(int a, int b) {
